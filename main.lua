c2d6484b99c11b29c19ce8dbb2d9ebea0e785d3a
Carolina Dias <carolinatdias@live.com>
2023-05-16 16:38:58 -0300

    setting virtual resolution

--- main.lua
+++ main.lua
@@ -1,6 +1,9 @@
 WINDOW_WIDTH = 1280 
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
 function love.load()
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
@@ -18,3 +21,4 @@ function love.draw()
         'center'
     )
 end
+
